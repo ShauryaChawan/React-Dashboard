bdb94659f22b35a9a92f8bf2a2cd252c25cf8a38
Shaurya Chawan <shauryachawan4646.dev@gmail.com>
2024-08-21 00:27:35 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Ract.js Dashboard - SixtyFour Data Intelligence LLP (India Data Hub) - Internshala
+
+## Frontend Tools
+1. React.js + Vite
+2. Styling - Sass (CSS)
---
+++ frontend/src/App.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div>
+      {/* Top */}
+        {/* Header */}
+        {/* Stats */}
+      {/* Body */}
+        {/* Location Card */}
+        {/* Top Sold Products Card */}
+        {/* Customer Details Card */}
+        {/* Daily Sales Trend Card */}
+    </div>
+  );
+};
+
+export default App;
