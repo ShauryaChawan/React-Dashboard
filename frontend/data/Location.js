73cb8188a0b22a6785deccfdc260f31e0dcabb75
Shaurya Chawan <shauryachawan4646.dev@gmail.com>
2024-08-22 22:47:13 +0530

    Added data to work with

---
+++ frontend/data/Location.js
@@ -0,0 +1,168 @@
+const QuantityLocation = [
+  {
+    id: "Location",
+    title: "Quantities sold by Location",
+    subTitle: "Top Location",
+    data: [
+      { id: 1, coutry: "United Kingdom", value: 276.3 },
+      { id: 2, coutry: "Netherlands", value: 14.8 },
+      { id: 3, coutry: "EIRE", value: 9.2 },
+      { id: 4, coutry: "France", value: 8.7 },
+      { id: 5, coutry: "Germany", value: 7.7 },
+      { id: 6, coutry: "Finland", value: 2.5 },
+      { id: 7, coutry: "Spain", value: 2.5 },
+      { id: 8, coutry: "Sweden", value: 5.4 },
+      { id: 9, coutry: "Denmark", value: 0.9 },
+      { id: 10, coutry: "Channel Islands", value: 2.1 },
+    ],
+  },
+  {
+    id: "Products",
+		title: "Top Sold Products",
+    data: [
+      {
+        id: 1,
+        product: "Jumbo bag red retrospot",
+        percentage: 1.46,
+        sales: 4.9,
+      },
+      {
+        id: 2,
+        product: "World war 2 gliders asstd designs",
+        percentage: 1.15,
+        sales: 3.9,
+      },
+      {
+        id: 3,
+        product: "Pack of 72 retrospot cake cases",
+        percentage: 1.1,
+        sales: 3.7,
+      },
+      {
+        id: 4,
+        product: "Pack of 12 london tissues",
+        percentage: 1.04,
+        sales: 3.5,
+      },
+      {
+        id: 5,
+        product: "Brocade ring purse",
+        percentage: 1.0,
+        sales: 3.4,
+      },
+      {
+        id: 6,
+        product: "Assorted colours silk fan",
+        percentage: 0.86,
+        sales: 2.9,
+      },
+      {
+        id: 7,
+        product: "Jumbo bag strawberry",
+        percentage: 0.84,
+        sales: 2.8,
+      },
+      {
+        id: 8,
+        product: "Mini paint set vintage",
+        percentage: 0.81,
+        sales: 2.7,
+      },
+      {
+        id: 9,
+        product: "Mini paint set vintage",
+        percentage: 0.81,
+        sales: 2.7,
+      },
+      {
+        id: 10,
+        product: "Mini paint set vintage",
+        percentage: 0.81,
+        sales: 2.6,
+      },
+      {
+        id: 11,
+        product: "Mini paint set vintage",
+        percentage: 0.81,
+        sales: 2.6,
+      },
+      {
+        id: 12,
+        product: "Mini paint set vintage",
+        percentage: 0.81,
+        sales: 2.5,
+      },
+      {
+        id: 13,
+        product: "Mini paint set vintage",
+        percentage: 0.81,
+        sales: 2.4,
+      },
+      {
+        id: 14,
+        product: "Mini paint set vintage",
+        percentage: 0.81,
+        sales: 2.3,
+      },
+      {
+        id: 15,
+        product: "Mini paint set vintage",
+        percentage: 0.81,
+        sales: 2.2,
+      },
+      {
+        id: 16,
+        product: "Mini paint set vintage",
+        percentage: 0.81,
+        sales: 2.2,
+      },
+      {
+        id: 17,
+        product: "Mini paint set vintage",
+        percentage: 0.81,
+        sales: 2.1,
+      },
+      {
+        id: 18,
+        product: "Mini paint set vintage",
+        percentage: 0.81,
+        sales: 2.0,
+      },
+      {
+        id: 19,
+        product: "Mini paint set vintage",
+        percentage: 0.81,
+        sales: 1.9,
+      },
+      {
+        id: 20,
+        product: "Mini paint set vintage",
+        percentage: 0.81,
+        sales: 1.8,
+      },
+    ],
+  },
+  {
+    id: "Details",
+		title: "Customer Details",
+		tableHeading:[
+			{id: 1, heading: "Customer ID"},
+			{id: 2, heading: "Country"},
+			{id: 3, heading: "Day of Latest Purchase"},
+			{id: 4, heading: "No. of Purchases"},
+			{id: 5, heading: "Sales"},
+		],
+		data:[
+			{
+				id: 14646,
+				country: "Nettherlands",
+				dolp: "30/3/2022",
+				purchases: 4,
+				sales: 21.5
+			},
+		]
+  },
+  {
+    id: "Trend",
+  },
+];
--- frontend/src/components/cards/customerDetailsCard/CustomerDetailsCard.jsx
+++ frontend/src/components/cards/customerDetailsCard/CustomerDetailsCard.jsx
@@ -1,10 +1,20 @@
-import React from 'react'
+import React from "react";
 import "./customerDetailsCard.scss";
+import customer_data from "../../../../data/customer_data.json";
 
 const customerDetailsCard = () => {
-	return (
-		<div>Customer Details Card</div>
-	)
-}
+  console.log(customer_data[0]);
 
-export default customerDetailsCard
+  return (
+    <div>
+      <h2>Customer Details</h2>
+			<table>
+				<tr>
+					<th>Customer ID</th>
+				</tr>
+			</table>
+    </div>
+  );
+};
+
+export default customerDetailsCard;
